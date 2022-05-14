ecd57ef706f3bbd8e9f81fe7404449588b4d1af9
Origogi <rlawjdxo1212@naver.com>
2022-05-14 18:56:57 +0900

    volumeCredits() 함수 재정의:

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -5,43 +5,41 @@ class PerformaceCalculator {
   }
 
   get amount() {
-    let result = 0;
-
-    switch (this.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르 : ${this.play.type}`);
-    }
-    return result;
+    throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
   }
 
   get volumeCredits() {
-    // 포인트를 적립한다.
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
 
-    if ('comedy' == this.play.type) {
-      result += Math.floor(this.performance.audience / 5);
+class TragedyCalculator extends PerformaceCalculator {
+  get amount() {
+    let result = 40000;
+
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
     }
+
     return result;
   }
 }
 
-class TragedyCalculator extends PerformaceCalculator {}
+class ComedyCalculator extends PerformaceCalculator {
+  get amount() {
+    let result = 30000;
 
-class ComedyCalculator extends PerformaceCalculator {}
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
 
 export default function createStatementData(invoice, plays) {
   const statementData = {};
@@ -71,7 +69,6 @@ export default function createStatementData(invoice, plays) {
       default:
         throw new Error(`알 수 없는 장르 ${aPlay.type}`);
     }
-
   }
 
   function playFor(aPerfomance) {
