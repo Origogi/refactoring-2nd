bd3cb783ebaab08623d51e55a7ba6f8bab49298d
Origogi <rlawjdxo1212@naver.com>
2022-08-21 15:06:06 +0900

    Update 7-9

--- ch7/after/7-9.js
+++ ch7/after/7-9.js
@@ -1,16 +1,7 @@
 function foundPerson(people) {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') {
-      return 'Don';
-    }
-    if (people[i] === 'John') {
-      return 'John';
-    }
-    if (people[i] === 'Kent') {
-      return 'Kent';
-    }
-  }
-  return '';
+  const candidate = ['Don', 'John', "Kent"]
+  return people.find((c) => candidate.includes(c)) || ''
+
 }
 
 console.log(foundPerson(['John']));
