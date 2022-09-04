4cbf50fa122fcb230c9ea84dd65d1c32bc179fa4
Origogi <rlawjdxo1212@naver.com>
2022-09-04 21:53:27 +0900

    Fix

---
+++ ch11/after/11-1.js
@@ -0,0 +1,30 @@
+// 예제 1
+function totalOutstanding() {
+  return customer.invoices.reduce((total, each) => each.amount + total, 0);
+}
+
+function sendBill() {
+  // ..
+}
+
+// 예제 2
+export function alertForMiscreant(people, alarm) {
+  const miscreant = findMiscreant(people)
+  setOffAlarms(alarm, miscreant)
+}
+
+function findMiscreant(people) {
+  for (const p of people) {
+    if (p === 'Don') {
+      return 'Don';
+    }
+    if (p === 'John') {
+      return 'John';
+    }
+  }
+  return '';
+}
+
+function setOffAlarms(alarm, p) {
+  alarm.setOff('Found Miscreant ' + p);
+}
