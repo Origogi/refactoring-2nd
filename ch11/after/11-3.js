ab14ba63e62fe554eca6dc378510bd379540a0b3
tony.origogi <tony.origogi@kakaobrain.com>
2022-09-17 10:47:27 +0900

    Update 11-3

---
+++ ch11/after/11-3.js
@@ -0,0 +1,18 @@
+// 예제 1
+function setHeight(value) {
+  this._height = value;
+}
+
+function setWidth(value) {
+  this._width = value;
+}
+
+// 예제 2
+class Concert {
+  regularBook(customer)
+  premiumBook(customer)
+  #book(customer,isPremium)
+}
+
+// 예제 3
+function swtichOn();
