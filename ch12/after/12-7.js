f2d312d858713010cb7703069a68d1a6eead8a30
Origogi <rlawjdxo1212@naver.com>
2022-09-19 21:54:52 +0900

    12-7

---
+++ ch12/after/12-7.js
@@ -0,0 +1,43 @@
+class Person {
+  #name;
+  #genderCode;
+  constructor(name, genderCode) {
+    this.#name = name;
+    this.#genderCode = genderCode;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get genderCode() {
+    return this.#genderCode;
+  }
+
+  get isMale() {
+    return this.#genderCode === 'M';
+  }
+
+  static create(record) {
+    switch (record.gender) {
+      case 'M':
+        return new Person(record.name, 'M');
+      case 'F':
+        return new Person(record.name, 'F');
+      default:
+        return new Person(record.name, 'X');
+    }
+  }
+}
+
+function loadFromInput(data) {
+  return data.map((record) => Person.create(record));
+}
+
+const people = loadFromInput([
+  { name: '엘리', gender: 'F' },
+  { name: '철수', gender: 'M' },
+  { name: '밥', gender: 'M' },
+]);
+const numberOfMales = people.filter((p) => p.isMale).length;
+console.log(numberOfMales);
