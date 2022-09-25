1a0f73c7ab25326103596864bad054e29937dc6a
Origogi <rlawjdxo1212@naver.com>
2022-09-25 17:57:38 +0900

    Add Perfomance class

--- challenge/tests/create_statement.js
+++ challenge/tests/create_statement.js
@@ -1,63 +1,69 @@
-export function createStatememt(invoice, plays) {
-    const result = {};
+class Performance {
+  #audience;
+  #play;
 
-    result.customer = invoice.customer;
-    result.performances = invoice.performances.map(enrichPerformance);
-    result.totalAmount = totalAmount(result.performances);
-    result.totalCredits = totalCredits(result.performances);
-
-    return result;
-
-    function totalAmount(performances) {
-      return performances.reduce((sum, perf) => perf.amount + sum, 0);
-    }
-  
-    function totalCredits(performances) {
-      return performances.map((perf) => perf.credits).reduce((a, b) => a + b, 0);
-    }
-  
-    function enrichPerformance(performance) {
-      const result = { ...performance };
-  
-      result.play = playFor(performance)
-      result.amount = amountFor(result);
-      result.credits = creditsFor(result);
-  
-      return result;
-    }
-  
-    function playFor(performance) {
-      return plays[performance.playID];
-    }
-  
-    function amountFor(performance) {
-      let thisAmount = 0;
-      switch (performance.play.type) {
-        case 'tragedy': // 비극
-          thisAmount = 40000;
-          if (performance.audience > 30) {
-            thisAmount += 1000 * (performance.audience - 30);
-          }
-          break;
-        case 'comedy': // 희극
-          thisAmount = 30000;
-          if (performance.audience > 20) {
-            thisAmount += 10000 + 500 * (performance.audience - 20);
-          }
-          thisAmount += 300 * performance.audience;
-          break;
-        default:
-          throw new Error(`알 수 없는 장르: ${play.type}`);
-      }
-      return thisAmount;
-    }
-  
-    function creditsFor(performance) {
-      let result = Math.max(performance.audience - 30, 0);
-      // 희극 관객 5명마다 추가 포인트를 제공한다.
-      if ('comedy' === performance.play.type) {
-        result += Math.floor(performance.audience / 5);
-      }
-      return result;
-    }
+  constructor(audience, play) {
+    this.#audience = audience;
+    this.#play = play;
   }
+
+  get audience() {
+    return this.#audience;
+  }
+
+  get play() {
+    return this.#play;
+  }
+
+  get amount() {
+    let thisAmount = 0;
+    switch (this.#play.type) {
+      case 'tragedy': // 비극
+        thisAmount = 40000;
+        if (this.#audience > 30) {
+          thisAmount += 1000 * (this.#audience - 30);
+        }
+        break;
+      case 'comedy': // 희극
+        thisAmount = 30000;
+        if (this.#audience > 20) {
+          thisAmount += 10000 + 500 * (this.#audience - 20);
+        }
+        thisAmount += 300 * this.#audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.#play.type}`);
+    }
+    return thisAmount;
+  }
+
+  get credits() {
+    let result = Math.max(this.#audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === this.#play.type) {
+      result += Math.floor(this.#audience / 5);
+    }
+    return result;
+  }
+}
+
+export function createStatememt(invoice, plays) {
+  const result = {};
+
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(
+    (p) => new Performance(p.audience, plays[p.playID])
+  );
+  result.totalAmount = totalAmount(result.performances);
+  result.totalCredits = totalCredits(result.performances);
+
+  return result;
+
+  function totalAmount(performances) {
+    return performances.reduce((sum, perf) => perf.amount + sum, 0);
+  }
+
+  function totalCredits(performances) {
+    return performances.map((perf) => perf.credits).reduce((a, b) => a + b, 0);
+  }
+}
